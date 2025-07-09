159a16d6be00432c062bd1631722ce8a265be04c
Tahmid Ahmed <tahmid@tahmid.io>
2025-07-09 00:36:12 -0400

    Enable CopilotChat

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,43 +1,42 @@
-return {}
-
--- return {
---   {
---     "CopilotC-Nvim/CopilotChat.nvim",
---     dependencies = {
---       { "zbirenbaum/copilot.lua" },
---       { "nvim-lua/plenary.nvim", branch = "master" },
---     },
---     build = "make tiktoken", -- Only on MacOS or Linux
---   },
---   {
---     "zbirenbaum/copilot.lua",
---     cmd = "Copilot",
---     event = "InsertEnter",
---     opts = {
---       suggestion = { enabled = false },
---       panel = { enabled = false },
---       filetypes = {
---         markdown = true,
---         help = true,
---       },
---     },
---   },
---   {
---     "saghen/blink.cmp",
---     optional = true,
---     dependencies = { "fang2hou/blink-copilot" },
---     opts = {
---       sources = {
---         default = { "copilot" },
---         providers = {
---           copilot = {
---             name = "copilot",
---             module = "blink-copilot",
---             score_offset = 100,
---             async = true,
---           },
---         },
---       },
---     },
---   },
--- }
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" },
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    opts = {},
+    build = "make tiktoken", -- Only on MacOS or Linux
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+  },
+  -- {
+  --   "saghen/blink.cmp",
+  --   optional = true,
+  --   dependencies = { "fang2hou/blink-copilot" },
+  --   opts = {
+  --     sources = {
+  --       default = { "copilot" },
+  --       providers = {
+  --         copilot = {
+  --           name = "copilot",
+  --           module = "blink-copilot",
+  --           score_offset = 100,
+  --           async = true,
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
+}
