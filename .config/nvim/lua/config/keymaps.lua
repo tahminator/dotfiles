2d74b2aff44170187ad8a8336881855ef882752a
Tahmid Ahmed <tahmid@tahmid.io>
2025-07-27 16:18:56 -0400

    Add nvim-spectre for multi-file search/replace

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -76,6 +76,19 @@ vim.keymap.set({ "n", "v" }, "c", '"_c', { noremap = true })
 vim.keymap.set("n", "x", "d", { noremap = true })
 vim.keymap.set("n", "xx", "dd", { noremap = true })
 
+vim.keymap.set("n", "<leader>S", '<cmd>lua require("spectre").toggle()<CR>', {
+  desc = "Toggle Spectre",
+})
+vim.keymap.set("n", "<leader>sw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+  desc = "Search current word",
+})
+vim.keymap.set("v", "<leader>sw", '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+  desc = "Search current word",
+})
+vim.keymap.set("n", "<leader>sp", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+  desc = "Search on current file",
+})
+
 vim.g.VM_custom_motions = {
   ["k"] = "j",
   ["j"] = "k",
