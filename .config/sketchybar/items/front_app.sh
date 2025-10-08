9d6ee9e2e4f30674011db8cbfac7b3b64c65ed42
Tahmid Ahmed <tahmid@tahmid.io>
2025-10-08 19:29:40 -0400

    Make font_app text smaller

--- .config/sketchybar/items/front_app.sh
+++ .config/sketchybar/items/front_app.sh
@@ -5,6 +5,6 @@ sketchybar --add item front_app left \
 	icon.color=$ITEM_COLOR \
 	label.color=$ITEM_COLOR \
 	icon.font="sketchybar-app-font:Regular:12.0" \
-	label.font="SF Pro:Semibold:12.0" \
+	label.font="SF Pro:Semibold:8.0" \
 	script="$PLUGIN_DIR/front_app.sh" \
 	--subscribe front_app front_app_switched
