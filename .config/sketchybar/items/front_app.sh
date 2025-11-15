d011ce7ccd3f988d34c566d08d34ff6a10627ff2
Tahmid Ahmed <tahmid@tahmid.io>
2025-11-15 04:03:08 -0500

    Make space icons + font little bigger

--- .config/sketchybar/items/front_app.sh
+++ .config/sketchybar/items/front_app.sh
@@ -4,7 +4,7 @@ sketchybar --add item front_app left \
 	--set front_app background.color=$ACCENT_COLOR \
 	icon.color=$ITEM_COLOR \
 	label.color=$ITEM_COLOR \
-	icon.font="sketchybar-app-font:Regular:12.0" \
-	label.font="SF Pro:Semibold:8.0" \
+	icon.font="sketchybar-app-font:Regular:14.0" \
+	label.font="SF Pro:Semibold:14.0" \
 	script="$PLUGIN_DIR/front_app.sh" \
 	--subscribe front_app front_app_switched
