3d2aeb16db8796e89c1cb27bc7b7b98141ee706b
Tahmid Ahmed <tahmid@tahmid.io>
2025-12-02 03:54:13 -0500

    Replace current theme with everforest

---
+++ .config/nvim/lua/plugins/everforest.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "neanias/everforest-nvim",
+    version = false,
+    lazy = false,
+    priority = 1000,
+    config = function(_, opts)
+      require("everforest").setup(opts)
+    end,
+    opts = {
+      background = "soft",
+      transparent_background_level = 2,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+    },
+  },
+}
--- .config/nvim/lua/plugins/nightfox-theme.lua
+++ .config/nvim/lua/plugins/nightfox-theme.lua
@@ -1,51 +1,53 @@
-return {
-  {
-    "EdenEast/nightfox.nvim",
-    opts = function()
-      require("nightfox").setup({
-        palettes = {
-          carbonfox = {
-            bg0 = "NONE", -- floating windows
-            -- bg3 = "NONE", -- tabline + project name in neotree
-          },
-        },
-        specs = {
-          carbonfox = {
-            syntax = {
-              keyword = "magenta.dim",
-              variable = "magenta",
-              comment = "white.dim",
-            },
-          },
-        },
-        groups = {
-          carbonfox = {
-            Visual = {
-              bg = "bg2",
-            },
-          },
-        },
-        options = {
-          transparent = true,
-        },
-      })
-    end,
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      -- colorscheme = "dayfox",
-      colorscheme = "carbonfox",
-      transparent = true,
-    },
-  },
-}
+return {}
 
--- available colorschemes:
--- carbonfox
--- terafox
--- nordfox
--- duskfox
--- dawnfox
--- dayfox
--- nightfox
+-- return {
+--   {
+--     "EdenEast/nightfox.nvim",
+--     opts = function()
+--       require("nightfox").setup({
+--         palettes = {
+--           carbonfox = {
+--             bg0 = "NONE", -- floating windows
+--             -- bg3 = "NONE", -- tabline + project name in neotree
+--           },
+--         },
+--         specs = {
+--           carbonfox = {
+--             syntax = {
+--               keyword = "magenta.dim",
+--               variable = "magenta",
+--               comment = "white.dim",
+--             },
+--           },
+--         },
+--         groups = {
+--           carbonfox = {
+--             Visual = {
+--               bg = "bg2",
+--             },
+--           },
+--         },
+--         options = {
+--           transparent = true,
+--         },
+--       })
+--     end,
+--   },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       -- colorscheme = "dayfox",
+--       colorscheme = "carbonfox",
+--       transparent = true,
+--     },
+--   },
+-- }
+--
+-- -- available colorschemes:
+-- -- carbonfox
+-- -- terafox
+-- -- nordfox
+-- -- duskfox
+-- -- dawnfox
+-- -- dayfox
+-- -- nightfox
