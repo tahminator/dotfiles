e7a6dd7bf172ea905764e5f3645e9fcba5725c47
Tahmid Ahmed <tahmid@tahmid.io>
2026-01-05 14:27:55 -0500

    tsgo (wip)

--- .config/nvim/lua/plugins/neovim-lspconfig.lua
+++ .config/nvim/lua/plugins/neovim-lspconfig.lua
@@ -37,6 +37,34 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      servers = {
+        tsserver = { enabled = false },
+        vtsls = { enabled = false },
+        tsgo = {
+          cmd = { "tsgo", "--lsp", "--stdio" },
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "javascript.jsx",
+            "typescript",
+            "typescriptreact",
+            "typescript.tsx",
+          },
+          root_markers = {
+            "tsconfig.json",
+            "jsconfig.json",
+            "package.json",
+            ".git",
+            "tsconfig.base.json",
+          },
+          enabled = true,
+        },
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
       inlay_hints = { enabled = false },
       diagnostics = {
         enable = enabledFlag,
