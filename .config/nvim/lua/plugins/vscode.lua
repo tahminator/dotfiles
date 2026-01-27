f6f488954ed14869b74f80aac78d549c5da3ec96
Tahmid Ahmed <tahmid@tahmid.io>
2026-01-27 14:11:53 -0500

    vscode theme

---
+++ .config/nvim/lua/plugins/vscode.lua
@@ -0,0 +1,15 @@
+local block = false
+if block then
+  return {}
+end
+
+return {
+  "shadowy-pycoder/arctic.nvim",
+  dependencies = { "rktjmp/lush.nvim" },
+  name = "arctic",
+  branch = "v2",
+  priority = 1000,
+  config = function()
+    vim.cmd("colorscheme arctic")
+  end,
+}
