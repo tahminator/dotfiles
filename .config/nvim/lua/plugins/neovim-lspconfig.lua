5188749f50629c64b956ad670751aa98e09e298b
Tahmid Ahmed <tahmid@tahmid.io>
2026-01-27 13:44:26 -0500

    add fixes for lspconfig opts warning

--- .config/nvim/lua/plugins/neovim-lspconfig.lua
+++ .config/nvim/lua/plugins/neovim-lspconfig.lua
@@ -8,31 +8,15 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    opts = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      if vim.g.started_by_firenvim == true then
-        keys[#keys + 1] = { "K", "" }
-      end
-      keys[#keys + 1] = { "<c-k>", false }
-      -- local client = vim.api.nvim_get_chan_info(vim.v.event.chan).client
-      -- if client ~= nil and client.name == "Firenvim" then
-      -- end
-      -- nvim-lspconfig don't handle this logic anymore. it's spread out between blink.cmp, noice/snacks, etc.
-      -- return {
-      --   defaults = {
-      --     mappings = {
-      --       i = {
-      --         ["<C-j>"] = "move_selection_previous", -- up
-      --         ["<C-k>"] = "move_selection_next", -- down
-      --       },
-      --       n = {
-      --         ["<C-j>"] = "move_selection_previous",
-      --         ["<C-k>"] = "move_selection_next",
-      --       },
-      --     },
-      --   },
-      -- }
-    end,
+    opts = {
+      servers = {
+        ["*"] = {
+          keys = {
+            { "<c-k>", false },
+          },
+        },
+      },
+    },
   },
   {
     "neovim/nvim-lspconfig",
