5b051705c6acc0a9be0d472a193dcc732fff4dd7
Tahmid Ahmed <tahmid@tahmid.io>
2026-03-24 03:08:54 -0400

    make binary copying more resilient

--- scripts/scripts.zsh
+++ scripts/scripts.zsh
@@ -2,30 +2,30 @@ SCRIPTS_DIR=$(eval echo "~/scripts/files")
 
 # no extensions, just filenames
 SCRIPTS=(
-  "gradle-fix"
-  "string-printer"
-  "recursive-git-pull"
-  "bitwarden"
-  "latex-compile"
-  "spotless"
-  "path"
-  "t"
-  "td"
-  "tk"
-  "tl"
-  "tlp"
-  "tlw"
-  "ts"
-  "launcher"
-  "desc"
+	"gradle-fix"
+	"string-printer"
+	"recursive-git-pull"
+	"bitwarden"
+	"latex-compile"
+	"spotless"
+	"path"
+	"t"
+	"td"
+	"tk"
+	"tl"
+	"tlp"
+	"tlw"
+	"ts"
+	"launcher"
+	"desc"
 )
 
 for script in "${SCRIPTS[@]}"; do
-    alias "$script"="$SCRIPTS_DIR/$script.sh"
+	alias "$script"="$SCRIPTS_DIR/$script.sh"
 done
 
 for script in "${SCRIPTS[@]}"; do
-    chmod +x "$SCRIPTS_DIR/$script.sh"
+	chmod +x "$SCRIPTS_DIR/$script.sh"
 done
 
 # extra short aliases below here
@@ -33,10 +33,12 @@ alias lc=latex-compile
 
 # move special binaries to bin
 BINARIES=(
-  # https://github.com/nkleemann/ascii-rain
+	# https://github.com/nkleemann/ascii-rain
 	"rain"
 )
 
 for binary in "${BINARIES[@]}"; do
-	cp "$binary" "/usr/local/bin/$binary"
+	if ! command -v "$binary" &>/dev/null; then
+		cp "$SCRIPTS_DIR/$binary" "/usr/local/bin/$binary"
+	fi
 done
